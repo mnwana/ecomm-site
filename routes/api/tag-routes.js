327aca0cbdf5f7b88452223ca79b231aa1c90a6c
mnwana <mariellenwana@gmail.com>
2022-09-06 19:28:22 -0400

    add tag destroy

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,6 +23,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData){
+      res.status(404).json({message: 'No tag found with this ID'});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
