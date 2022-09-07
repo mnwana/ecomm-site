fd99cdac4ca8c710245e4acc0ec8160471a2e1b1
mnwana <mariellenwana@gmail.com>
2022-09-06 20:41:13 -0400

    add destroy product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -91,6 +91,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No product found with this ID" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
